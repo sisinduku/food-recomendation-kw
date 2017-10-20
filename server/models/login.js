527f056992942deabcd903052e92c637fd795927
sisinduku <saptanto.sindu@gmail.com>
2017-10-20 10:56:40 +0700

    Add Integration Client with Server

--- server/models/login.js
+++ server/models/login.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const fb = require('fb');
+const FB = require('fb');
 
 class Login {
   static login(req, res, next) {
@@ -11,12 +11,13 @@ class Login {
     fb.api(req.body.userId, function(response) {
       if (response.error) {
         res.status(400).json(response.error);
+      } else {
+        var token = jwt.sign({
+          id: response.id,
+          name: response.name,
+        }, process.env.APPSECRET);
+        res.status(200).json(token);
       }
-      var token = jwt.sign({
-        id: response.id,
-        name: response.name,
-      }, process.env.APPSECRET);
-      res.status(200).json(token);
     });
   }
 }
